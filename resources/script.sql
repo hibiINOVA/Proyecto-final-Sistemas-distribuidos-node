04c149db506f1548ac795dab3f994f88147cab12
hibi I NOVA <84207744+hibiINOVA@users.noreply.github.com>
2025-12-11 20:09:22 -0600

    Insert sample data into usuarios table
    Added sample user data to the usuarios table.

--- resources/script.sql
+++ resources/script.sql
@@ -9,3 +9,30 @@ CREATE TABLE usuarios (
     fecha_ultima_sesion TIMESTAMP NULL,
     activo BOOLEAN DEFAULT TRUE
 );
+
+INSERT INTO usuarios (
+    id, nombre, apellidos, email, password, telefono,
+    fecha_registro, fecha_ultima_sesion, activo
+) VALUES
+(
+    '0b1f6c1f-0609-4778-93d9-0ee4462eb80f',
+    'jose',
+    'garcia morales',
+    'conoplant2@gmail.com',
+    '$2b$10$zHWujMFZUH3NyXFNZZsyduoPxvwu/s.4yDh9eRWYLfRioJwt4bYqq',
+    '5551234567',
+    '2025-12-09 02:28:30',
+    '2025-12-09 07:25:44',
+    1
+),
+(
+    'd1be722a-f1f9-43de-9557-52d6975e3ced',
+    'jose manuel',
+    'garcia morales',
+    'hibiynova@gmail.com',
+    '$2b$10$R2YkCAUo16IyUQhzh9boP.in9AgpA2Ug/n9V0iXqg4GPaKfhzP0.u',
+    '4151445758',
+    '2025-12-11 02:55:52',
+    '2025-12-11 02:57:27',
+    1
+);
